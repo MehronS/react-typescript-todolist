cb004b0d0febad71573edd07068e58617693c136
Mehron Latifi <88285109+MehronS@users.noreply.github.com>
2022-01-14 23:41:28 -0500

    testing out React with typescript

---
+++ src/components/NewTask.tsx
@@ -0,0 +1,32 @@
+import React, { useRef } from "react";
+import "./styles.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+export const NewTask = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        placeholder="Enter Task"
+        className="input-box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input-submit">Add</button>
+    </form>
+  );
+};
